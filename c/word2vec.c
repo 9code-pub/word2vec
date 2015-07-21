020bdd77687fc77dde882f3d889c487d66b5a7d4
Halo9Pan <halo9pan@gmail.com>
2015-07-21 18:52:07 +0800

    Reading code, add comment

--- c/word2vec.c
+++ c/word2vec.c
@@ -36,9 +36,11 @@ typedef float real;                    // Precision of float numbers
 
 struct vocab_word
 {
-  long long frequency; //词频
+  int64_t frequency; //词频
   int *point; //Huffman编码对应内节点的路径
-  char *word, *code, codelen; //Huffman编码
+  char *word; //词内容
+  char *code; //Huffman编码
+  int8_t codelen; //Huffman编码长度
 };
 
 char train_file[MAX_STRING], output_file[MAX_STRING];
@@ -46,8 +48,8 @@ char save_vocab_file[MAX_STRING], read_vocab_file[MAX_STRING];
 struct vocab_word *vocab;
 int binary = 0, cbow = 1, debug_mode = 2, window = 5, min_count = 5, num_threads = 12, min_reduce = 1;
 int *vocabulary_hash;
-long long vocabulary_max_size = 1000, vocabulary_size = 0, layer1_size = 100;
-long long train_words = 0, word_count_actual = 0, iter = 5, file_size = 0, classes = 0;
+int64_t vocabulary_max_size = 1000, vocabulary_size = 0, layer1_size = 100;
+int64_t train_words = 0, word_count_actual = 0, iter = 5, file_size = 0, classes = 0;
 real alpha = 0.025, starting_alpha, sample = 1e-3;
 real *syn0, *syn1, *syn1neg, *expTable;
 clock_t start;
@@ -233,56 +235,59 @@ sort_vocabulary ()
 }
 
 // Reduces the vocabulary by removing infrequent tokens
+// 再次移除词频过小的词，缩减词汇表
 void
-ReduceVocab ()
+reduce_vocabulary ()
 {
-  int a, b = 0;
+  int i, j = 0;
   uint32_t hash;
-  for (a = 0; a < vocabulary_size; a++)
-	if (vocab[a].frequency > min_reduce)
-	  {
-		vocab[b].frequency = vocab[a].frequency;
-		vocab[b].word = vocab[a].word;
-		b++;
-	  }
-	else
-	  free (vocab[a].word);
-  vocabulary_size = b;
-  for (a = 0; a < vocabulary_hash_size; a++)
-	vocabulary_hash[a] = -1;
-  for (a = 0; a < vocabulary_size; a++)
+  for (i = 0; i < vocabulary_size; i++) // 双游标压缩
+	{
+	  if (vocab[i].frequency > min_reduce)
+		{
+		  vocab[j].frequency = vocab[i].frequency;
+		  vocab[j].word = vocab[i].word;
+		  j++;
+		}
+	  else
+		free (vocab[i].word); // 删除结构体内的内容
+	}
+  vocabulary_size = j;
+  for (i = 0; i < vocabulary_hash_size; i++) // 重新计算hash值
+	vocabulary_hash[i] = -1;
+  for (i = 0; i < vocabulary_size; i++)
 	{
 	  // Hash will be re-computed, as it is not actual
-	  hash = get_word_hash (vocab[a].word);
+	  hash = get_word_hash (vocab[i].word);
 	  while (vocabulary_hash[hash] != -1)
 		hash = (hash + 1) % vocabulary_hash_size;
-	  vocabulary_hash[hash] = a;
+	  vocabulary_hash[hash] = i;
 	}
   fflush (stdout);
-  min_reduce++;
+  min_reduce++; // 每次移除词频过小的词，自增一下
 }
 
 // Create binary Huffman tree using the word counts
 // Frequent words will have short uniqe binary codes
 void
-CreateBinaryTree ()
+create_binary_tree ()
 {
-  long long a, b, i, min1i, min2i, pos1, pos2, point[MAX_CODE_LENGTH];
+  int64_t i, j, i, min1i, min2i, pos1, pos2, point[MAX_CODE_LENGTH];
   char code[MAX_CODE_LENGTH];
-  long long *count = (long long *) calloc (vocabulary_size * 2 + 1, sizeof(long long));
-  long long *binary = (long long *) calloc (vocabulary_size * 2 + 1, sizeof(long long));
-  long long *parent_node = (long long *) calloc (vocabulary_size * 2 + 1, sizeof(long long));
-  for (a = 0; a < vocabulary_size; a++)
-	count[a] = vocab[a].frequency;
-  for (a = vocabulary_size; a < vocabulary_size * 2; a++)
-	count[a] = 1e15;
+  int64_t *count = (int64_t *) calloc (vocabulary_size * 2 + 1, sizeof(int64_t)); // calloc时置零
+  int64_t *binary = (int64_t *) calloc (vocabulary_size * 2 + 1, sizeof(int64_t));
+  int64_t *parent_node = (int64_t *) calloc (vocabulary_size * 2 + 1, sizeof(int64_t));
+  for (i = 0; i < vocabulary_size; i++)
+	count[i] = vocab[i].frequency; // 前半段词频
+  for (i = vocabulary_size; i < vocabulary_size * 2; i++)
+	count[i] = 1e15; // 后半段大值
   pos1 = vocabulary_size - 1;
   pos2 = vocabulary_size;
   // Following algorithm constructs the Huffman tree by adding one node at a time
-  for (a = 0; a < vocabulary_size - 1; a++)
+  for (i = 0; i < vocabulary_size - 1; i++)
 	{
 	  // First, find two smallest nodes 'min1, min2'
-	  if (pos1 >= 0)
+	  if (pos1 >= 0) // 在count数组范围内查找
 		{
 		  if (count[pos1] < count[pos2])
 			{
@@ -300,7 +305,7 @@ CreateBinaryTree ()
 		  min1i = pos2;
 		  pos2++;
 		}
-	  if (pos1 >= 0)
+	  if (pos1 >= 0) // 在count数组范围内查找
 		{
 		  if (count[pos1] < count[pos2])
 			{
@@ -318,31 +323,31 @@ CreateBinaryTree ()
 		  min2i = pos2;
 		  pos2++;
 		}
-	  count[vocabulary_size + a] = count[min1i] + count[min2i];
-	  parent_node[min1i] = vocabulary_size + a;
-	  parent_node[min2i] = vocabulary_size + a;
-	  binary[min2i] = 1;
+	  count[vocabulary_size + i] = count[min1i] + count[min2i];
+	  parent_node[min1i] = vocabulary_size + i;
+	  parent_node[min2i] = vocabulary_size + i;
+	  binary[min2i] = 1; // 右子树为叶子节点
 	}
   // Now assign binary code to each vocabulary word
-  for (a = 0; a < vocabulary_size; a++)
+  for (i = 0; i < vocabulary_size; i++)
 	{
-	  b = a;
+	  j = i;
 	  i = 0;
 	  while (1)
 		{
-		  code[i] = binary[b];
-		  point[i] = b;
+		  code[i] = binary[j]; // 0或者1
+		  point[i] = j; // 路径
 		  i++;
-		  b = parent_node[b];
-		  if (b == vocabulary_size * 2 - 2)
+		  j = parent_node[j];
+		  if (j == vocabulary_size * 2 - 2)
 			break;
 		}
-	  vocab[a].codelen = i;
-	  vocab[a].point[0] = vocabulary_size - 2;
-	  for (b = 0; b < i; b++)
+	  vocab[i].codelen = i; // 树高
+	  vocab[i].point[0] = vocabulary_size - 2;
+	  for (j = 0; j < i; j++)
 		{
-		  vocab[a].code[i - b - 1] = code[b];
-		  vocab[a].point[i - b] = point[b] - vocabulary_size;
+		  vocab[i].code[i - j - 1] = code[j];
+		  vocab[i].point[i - j] = point[j] - vocabulary_size;
 		}
 	}
   free (count);
@@ -355,7 +360,7 @@ LearnVocabFromTrainFile ()
 {
   char word[MAX_STRING];
   FILE *fin;
-  long long a, i;
+  int64_t a, i;
   for (a = 0; a < vocabulary_hash_size; a++)
 	vocabulary_hash[a] = -1;
   fin = fopen (train_file, "rb");
@@ -386,7 +391,7 @@ LearnVocabFromTrainFile ()
 	  else
 		vocab[i].frequency++;
 	  if (vocabulary_size > vocabulary_hash_size * 0.7)
-		ReduceVocab ();
+		reduce_vocabulary ();
 	}
   sort_vocabulary ();
   if (debug_mode > 0)
@@ -401,7 +406,7 @@ LearnVocabFromTrainFile ()
 void
 SaveVocab ()
 {
-  long long i;
+  int64_t i;
   FILE *fo = fopen (save_vocab_file, "wb");
   for (i = 0; i < vocabulary_size; i++)
 	fprintf (fo, "%s %lld\n", vocab[i].word, vocab[i].frequency);
@@ -411,7 +416,7 @@ SaveVocab ()
 void
 ReadVocab ()
 {
-  long long a, i = 0;
+  int64_t a, i = 0;
   char c;
   char word[MAX_STRING];
   FILE *fin = fopen (read_vocab_file, "rb");
@@ -452,7 +457,7 @@ ReadVocab ()
 void
 InitNet ()
 {
-  long long a, b;
+  int64_t a, b;
   uint64_t next_random = 1;
   size_t size = (size_t) vocabulary_size * layer1_size * sizeof(real);
   printf("size: %I64u\n", size);
@@ -493,22 +498,22 @@ InitNet ()
 		next_random = next_random * (uint64_t) 25214903917 + 11;
 		syn0[a * layer1_size + b] = (((next_random & 0xFFFF) / (real) 65536) - 0.5) / layer1_size;
 	  }
-  CreateBinaryTree ();
+  create_binary_tree();
 }
 
 void *
 TrainModelThread (void *id)
 {
-  long long a, b, d, cw, word, last_word, sentence_length = 0, sentence_position = 0;
-  long long word_count = 0, last_word_count = 0, sen[MAX_SENTENCE_LENGTH + 1];
-  long long l1, l2, c, target, label, local_iter = iter;
-  uint64_t next_random = (long long) id;
+  int64_t a, b, d, cw, word, last_word, sentence_length = 0, sentence_position = 0;
+  int64_t word_count = 0, last_word_count = 0, sen[MAX_SENTENCE_LENGTH + 1];
+  int64_t l1, l2, c, target, label, local_iter = iter;
+  uint64_t next_random = (int64_t) id;
   real f, g;
   clock_t now;
   real *neu1 = (real *) calloc (layer1_size, sizeof(real));
   real *neu1e = (real *) calloc (layer1_size, sizeof(real));
   FILE *fi = fopen (train_file, "rb");
-  fseek (fi, file_size / (long long) num_threads * (long long) id, SEEK_SET);
+  fseek (fi, file_size / (int64_t) num_threads * (int64_t) id, SEEK_SET);
   while (1)
 	{
 	  if (word_count - last_word_count > 10000)
@@ -564,7 +569,7 @@ TrainModelThread (void *id)
 		  word_count = 0;
 		  last_word_count = 0;
 		  sentence_length = 0;
-		  fseek (fi, file_size / (long long) num_threads * (long long) id,
+		  fseek (fi, file_size / (int64_t) num_threads * (int64_t) id,
 		  SEEK_SET);
 		  continue;
 		}
